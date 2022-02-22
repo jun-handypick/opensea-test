351189cf47af4255bd14888b65ece3b0e2a13303
Jun <jun@handypick.io>
2022-02-22 09:51:36 +0900

    no message

--- out/AlienKIP17Token_ADDR.js
+++ out/AlienKIP17Token_ADDR.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = "0xCA17eA0f9B43c602fb0a805F07E5BfE44Dd34FFd";
+exports.default = "0x79F61237e4Ed2528C5b8F7F0D1C20E9d839f449F";
 //# sourceMappingURL=AlienKIP17Token_ADDR.js.map
